02ef60cbd9bc9b961fc021bf51c4c29fc2e1b989
Kamoru AbdulRasak Oyinlola <rkamoru1@gmail.com>
2025-09-11 14:45:06 +0100

    Practise Python Dictionary

---
+++ Python Data Types/0.5 Dictionaries.py
@@ -0,0 +1,11 @@
+#Dictionaries
+dict_list = {'Num_of_chair':4, 'Num_list':[1,2,3,4,5], 'Mosque':{'Water_Tap':12}}
+print(dict_list)
+print(dict_list['Num_of_chair'])
+print(dict_list['Num_list'][3])
+print(dict_list['Mosque']['Water_Tap'])
+
+#Dictionaries Method
+print(dict_list.keys())
+print(dict_list.values())
+print(dict_list.items())
