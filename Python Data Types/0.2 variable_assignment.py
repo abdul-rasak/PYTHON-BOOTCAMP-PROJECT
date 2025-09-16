01c3fdc7fc65aa7e09b49c92720c17903d29b953
Kamoru AbdulRasak Oyinlola <rkamoru1@gmail.com>
2025-09-16 15:12:15 +0100

    Works on Tuples

--- Python Data Types/0.2 variable_assignment.py
+++ Python Data Types/0.2 variable_assignment.py
@@ -1,10 +1,10 @@
 #Variable Assignment
-total_store = 4;
-numbers_of_tyres_per_store= 500;
+total_store = 4
+numbers_of_tyres_per_store= 500
 
-total_tyres_in_all_stores = numbers_of_tyres_per_store * total_store;
+total_tyres_in_all_stores = numbers_of_tyres_per_store * total_store
 
-print(total_tyres_in_all_stores);
+print(total_tyres_in_all_stores)
 
 #Typeof Practise
-print(type(total_tyres_in_all_stores));
+print(type(total_tyres_in_all_stores))
--- Python Data Types/0.3 string.py
+++ Python Data Types/0.3 string.py
@@ -1,29 +1,29 @@
 #string
 my_parrot_name = 'alex'
-#print(my_parrot_name)
+print(my_parrot_name)
 
 #The '\n' next_line sign
-#print("What's yoor name? \n My name is AbdulRasak");
+print("What's yoor name? \n My name is AbdulRasak");
 
 #The '\t' tab sign
-#print("What's up \t I'm good");
+print("What's up \t I'm good");
 
 #The 'len() function'
-#print(len(my_parrot_name));
+print(len(my_parrot_name));
 
 #string slicing
 company_name_1 = "Microsoft"
 company_name_2 = "Google"
 
-#print(company_name_1[:5])
-#print(company_name_2[:2])
-#print(company_name_1[5:])
-#print(company_name_2[1:5])
-#print(company_name_1[::2])
-#print(company_name_2[0:5:2])
-#print(company_name_2[0:5])
+print(company_name_1[:5])
+print(company_name_2[:2])
+print(company_name_1[5:])
+print(company_name_2[1:5])
+print(company_name_1[::2])
+print(company_name_2[0:5:2])
+print(company_name_2[0:5])
 #Reverse the string
-#print(company_name_1[::-1])
+print(company_name_1[::-1])
 
 #String concatination
 my_full_name = 'KamoruAbdulRasakOyinlola'
@@ -32,15 +32,15 @@ my_first_name = my_full_name[6:16]
 my_last_name = my_full_name[16:]
 full_name = my_surename + " " + my_first_name + " " + my_last_name;
 
-#print(full_name)
+print(full_name)
 
 #String methods
-   #print(full_name.upper())
-   #print(full_name.lower())
+print(full_name.upper())
+print(full_name.lower())
 
 #String formarting
-#print("My name is {} {} {}".format('Kamoru', 'AbdulRasak', 'Oyinlola' ))
-#print("My {T} name is {N}".format(T = "dog", N = "Alex"))
+print("My name is {} {} {}".format('Kamoru', 'AbdulRasak', 'Oyinlola' ))
+print("My {T} name is {N}".format(T = "dog", N = "Alex"))
 
 answer = 3.2589643
 print("The answer is {a:1.3f}".format(a = answer))
