27490afda37a4d9b870a5efbadbabd18cc685936
Kamoru AbdulRasak Oyinlola <rkamoru1@gmail.com>
2025-09-28 17:24:35 +0100

    add comparison operator file

---
+++ 2. Python Comparison Operators/0.1 comparison operators.py
@@ -0,0 +1,15 @@
+# Equlity Operator
+print(5==5) 
+print('a'=='b')
+
+#Inequlity Operator
+print(5 != 5)
+print('a' != 'b')
+
+#Greater Than and Less Than Operators
+print(2 > 1)
+print(10 < 20)
+
+#Greater Than or Equal To and Less Than or Equal To Operators
+print(2 >= 1)
+print(3 <= 3)
---
+++ 2. Python Comparison Operators/0.2 chaining comparison operators.py
@@ -0,0 +1,12 @@
+# and logic operators
+print((2 == 2) and ('a' == 'a'))
+print(('a' != 'a') and ('c' != 'b'))
+
+#or logic operator
+first_born_name = "Kamoru Barakat"
+second_born_name = "Kamoru Abdulrasak"
+third_born_name = "Olayemi Daniel"
+print((first_born_name[:6] == second_born_name[:6]) or (second_born_name[:6] == third_born_name[:7]))
+
+#not logic operator 
+print(not 1 == 1)
