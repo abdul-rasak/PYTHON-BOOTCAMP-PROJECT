c47f1da90b5f56705703d13d4c5350ddbabf9ec9
Kamoru AbdulRasak Oyinlola <rkamoru1@gmail.com>
2025-10-02 16:15:14 +0100

    add if,elif,else statement file

---
+++ 3. Python Statement/0.1 if,elif and else Statement.py
@@ -0,0 +1,9 @@
+#if, elif and else conditional statement
+company_name = 'Microsoft'
+if company_name == 'Google':
+    print("I'm currently working at google")
+elif company_name == 'Apple':
+    print("I'm currentl working at apple, am also a lover of their product")
+else :
+    print(f"Hey guy's i have just secure a job at {company_name}")
+           
